880801a4507a184c69009f82ff2232ca7fef082b
Kundan Kumar <126123305+kundan344@users.noreply.github.com>
2024-09-09 20:33:36 +0530

    Create backend.tf

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-eks-bucket-for-state-lock"
+    region         = "us-east-2"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
